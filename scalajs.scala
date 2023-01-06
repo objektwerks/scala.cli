63553cfae271afadb5d007244cb2dd56e1972c6b
objektwerks <objektwerks@runbox.com>
2023-01-06 13:16:26 -0500

    added scalajs.scala

---
+++ scalajs.scala
@@ -0,0 +1,9 @@
+package objektwerks
+
+import scala.scalajs.js
+
+object Hello:
+  def main(args: Array[String]): Unit =
+    val console = js.Dynamic.global.console
+    val message = "I am a ScalaJs source file and app!"
+    console.log(message)
